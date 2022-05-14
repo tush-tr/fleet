c363af50ffe48df5a982f429c5ed4e0df7abbb08
tush-tr <tusharrajput604@gmail.com>
2022-05-15 02:32:17 +0530

    update

--- routes/fleets.js
+++ routes/fleets.js
@@ -1,12 +1,11 @@
-module.exports = () => {
-    'use strict';
-    const express = require("express");
-    var fleetRoute = express.Router();
-    fleetRoute.route("/fleets")
-    .get((req,res)=>{
+const express = require("express");
+var fleetRoute = express.Router();
+
+fleetRoute.route("/fleets")
+    .get((req, res) => {
         res.render("fleets", { fleets: fleetsObj })
-    }).post((req,res)=>{
+    }).post((req, res) => {
         res.redirect("/addfleet")
     })
-    return fleetRoute;
-};
+
+module.exports = fleetRoute
